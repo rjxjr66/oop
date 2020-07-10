61a83498c3683f09ea19c2653703a649bb6cbf4d
hansol <rjxjr55@gmail.com>
2020-07-10 14:18:58 +0900

    remove unused import

--- src/main/java/test/Renderer.java
+++ src/main/java/test/Renderer.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import java.util.Vector;
 
 import javax.swing.JFrame;
 
